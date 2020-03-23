a09c9619111849ea646b1b6269b728ab811a95e0
unknown <monmaramba@yahoo.com>
2020-03-22 21:28:01 -0700

    styles for video, videowrapper, playlistheader and nightmode completed

--- src/components/containers/Player.js
+++ src/components/containers/Player.js
@@ -1,15 +1,40 @@
 import React from "react";
+// for uniform themes across the whole app passing it as a prop
+import { ThemeProvider } from "styled-components";
+import StyledPlayer from "../styles/StyledPlayer";
 
 import Video from "../Video";
 import Playlist from "../containers/Playlist";
 
+// creating the theme(just a regular js object)
+const theme = {
+  bgcolor: "#353535",
+  bgcolorItem: "#414141",
+  bgcolorItemActive: "#405c63",
+  bgcolorPlayed: "#526d4e",
+  border: "none",
+  borderPlayed: "none",
+  color: "fff"
+};
+const themeLight = {
+  bgcolor: "#fff",
+  bgcolorItem: "#fff",
+  bgcolorItemActive: "#80a7b1",
+  bgcolorPlayed: "#7d9979",
+  border: "none",
+  borderPlayed: "none",
+  color: "#353535"
+};
+
 const Player = props => {
   return (
+    <ThemeProvider theme={state.nightMode ? theme : themeLight}>
+      <StyledPlayer>
+        <Video />
+        <Playlist />
+      </StyledPlayer>
+    </ThemeProvider>
     // enclosing the components in a fragment <React.Fragment> shortened to <> and closed with </>
-    <>
-      <Video />
-      <Playlist />
-    </>
   );
 };
 
--- src/components/styles/StyledPlayer.js
+++ src/components/styles/StyledPlayer.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
-const StyledWbnPlayer = styled.div`
-  background: ${props => props.theme.bgcolor}; /* #353535; */
+const StyledPlayer = styled.div`
+  background: ${props => props.theme.bgcolor};
   border: ${props => props.theme.border};
   max-width: 1800px;
   margin: 0 auto;
@@ -11,10 +11,10 @@ const StyledWbnPlayer = styled.div`
   flex-direction: row;
   max-height: 863px;
   transition: all 0.5s ease;
-  @media screen and (max-width: 1400px) {
+
+  @media screen and(max-width: 1400px) {
     display: block;
     max-height: 10000px;
   }
 `;
-
-export default StyledWbnPlayer;
+export default StyledPlayer;
