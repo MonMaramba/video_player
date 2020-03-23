d3fecd8246d4836e5b47aa150f56cca1b1d3835d
unknown <monmaramba@yahoo.com>
2020-03-22 18:17:04 -0700

    home route and global style in styled components created

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import Player from "./Player";
 
-const App = () => <Player />;
+import GlobalStyle from "../styles/GlobalStyle";
+
+const App = () => (
+  <BrowserRouter>
+    <>
+      <Switch>
+        <Route exact path="/" component={Player} />
+        {/* :activeVideo creates a variable  */}
+        <Route exact path="/:activeVideo" component={Player} />
+      </Switch>
+      <GlobalStyle />
+    </>
+  </BrowserRouter>
+);
 
 export default App;
